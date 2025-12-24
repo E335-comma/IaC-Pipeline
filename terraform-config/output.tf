8570d3755db986e97b66ed9b1880c77d7e3ae903
E335-comma <ifeoluwaaderoju6@gmail.com>
2025-12-24 12:52:59 +0100

    Update

--- terraform-config/output.tf
+++ terraform-config/output.tf
@@ -1,7 +1,7 @@
-output "private_key" {
-    value = tls_private_key.ssh_key.private_key_pem
-    sensitive = true
-}
+#output "private_key" {
+#    value = tls_private_key.ssh_key.private_key_pem
+#    sensitive = true
+#}
 
 output "ec2_public_ip" {
     value = aws_instance.web.public_ip

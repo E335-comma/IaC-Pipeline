ad3bd92e25c2794d797b24853ca2c65580f88453
E335-comma <ifeoluwaaderoju6@gmail.com>
2026-01-08 21:53:16 +0100

    Attach aws ssm role to instance

--- terraform-config/compute.tf
+++ terraform-config/compute.tf
@@ -1,16 +1,35 @@
-data "aws_iam_role" "ssm_role" {
-  name = "AWSSSMManagedInstanceCore"
+resource "aws_iam_role" "ssm_role" {
+  name = "SSMRoleForEC2"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statment = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
-resource "aws_iam_instance_profile" "ssm_profile" {
-  name = "ssm-instance-profile"
-  role = data.aws_iam_role.ssm_role.name
+resource "aws_iam_role_policy_attachment" "ssm_policy_attach" {
+  role = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm_instance_profile" {
+  name = "SSMInstanceProfile"
+  role = aws_iam_role.ssm_role.name
 }
 
 resource "aws_instance" "web" {
   ami           = var.web_instance_ami
   instance_type = "t3.micro"
-  iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
+#  iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
+  role = aws_iam_role.ssm_role.name
   subnet_id     = aws_subnet.public_subnet.id
   security_groups = [aws_security_group.ec2_sg.id]
 
